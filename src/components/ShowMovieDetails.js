59924d7bb9f44fdb00669e02be55935c93464c07
Sabrina Lucero <sabrinita185@gmail.com>
2020-02-27 12:50:17 -0300

    Modify redirect path after delete a favourite movie

--- src/components/ShowMovieDetails.js
+++ src/components/ShowMovieDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import '../App.css';
 import axios from 'axios';
@@ -10,7 +10,7 @@ class showMovieDetails extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movie: {}
+      movie: {},
     };
   }
 
@@ -29,30 +29,29 @@ class showMovieDetails extends Component {
     //   .catch(err => {
     //     console.log("Error from ShowMovietails");
     //   })
-  };
+  }
 
-  onDeleteClick (id) {
+  onDeleteClick(id) {
     axios
-      .delete('http://localhost:8082/api/favorites-movies/'+id)
+      .delete('http://localhost:8082/api/favorites-movies/' + id)
       .then(res => {
-        this.props.history.push("/");
+        this.props.history.push('/show-favourite');
       })
       .catch(err => {
-        console.log("Error form ShowMovieDetails_deleteClick");
-      })
-  };
-
+        console.log('Error form ShowMovieDetails_deleteClick');
+      });
+  }
 
   render() {
-
     // const movie = this.state.movie;
-    const {movie} = this.props;
+    const { movie } = this.props;
 
     if (!movie) return null;
 
-    let MovieItem = <div>
-      <table className="table table-hover table-dark">
-        {/* <thead>
+    let MovieItem = (
+      <div>
+        <table className='table table-hover table-dark'>
+          {/* <thead>
           <tr>
             <th scope="col">#</th>
             <th scope="col">First</th>
@@ -60,94 +59,98 @@ class showMovieDetails extends Component {
             <th scope="col">Handle</th>
           </tr>
         </thead> */}
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Movie name</td>
-            <td>{ movie.moviename }</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Genre</td>
-            <td>{ movie.genre }</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Description</td>
-            <td>{ movie.description }</td>
-          </tr>
-          <tr>
-            <th scope="row">4</th>
-            <td>Year</td>
-            <td>{ movie.year }</td>
-          </tr>
-          <tr>
-            <th scope="row">5</th>
-            <td>Cast</td>
-            <td>{ movie.cast }</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+          <tbody>
+            <tr>
+              <th scope='row'>1</th>
+              <td>Movie name</td>
+              <td>{movie.moviename}</td>
+            </tr>
+            <tr>
+              <th scope='row'>2</th>
+              <td>Genre</td>
+              <td>{movie.genre}</td>
+            </tr>
+            <tr>
+              <th scope='row'>3</th>
+              <td>Description</td>
+              <td>{movie.description}</td>
+            </tr>
+            <tr>
+              <th scope='row'>4</th>
+              <td>Year</td>
+              <td>{movie.year}</td>
+            </tr>
+            <tr>
+              <th scope='row'>5</th>
+              <td>Cast</td>
+              <td>{movie.cast}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
 
     return (
-      <div className="ShowMovieDetails">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-10 m-auto">
+      <div className='ShowMovieDetails'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-10 m-auto'>
               <br /> <br />
-              <Link to="/" className="btn btn-outline-warning float-left">
-                  Show Movie List
+              <Link to='/' className='btn btn-outline-warning float-left'>
+                Show Movie List
               </Link>
             </div>
             <br />
-            <div className="col-md-8 m-auto">
-              <h1 className="display-4 text-center">Movie's Record</h1>
-              <p className="lead text-center">
-                  View Movie's Info
-              </p>
+            <div className='col-md-8 m-auto'>
+              <h1 className='display-4 text-center'>Movie's Record</h1>
+              <p className='lead text-center'>View Movie's Info</p>
               <hr /> <br />
             </div>
           </div>
-          <div>
-            { MovieItem }
-          </div>
+          <div>{MovieItem}</div>
 
-          <div className="row">
-            <div className="col-md-6">
-              <button type="button" className="btn btn-outline-danger btn-lg btn-block" onClick={this.onDeleteClick.bind(this,movie._id)}>Delete Movie</button><br />
-            </div>
-
-            <div className="col-md-6">
-              <Link to={`/edit-movie/${movie._id}`} className="btn btn-outline-info btn-lg btn-block">
-                    Edit Movie
-              </Link>
+          <div className='row'>
+            <div className='col-md-6'>
+              <button
+                type='button'
+                className='btn btn-outline-danger btn-lg btn-block'
+                onClick={this.onDeleteClick.bind(this, movie._id)}
+              >
+                Delete Movie
+              </button>
               <br />
             </div>
 
+            <div className='col-md-6'>
+              <Link
+                to={`/edit-movie/${movie._id}`}
+                className='btn btn-outline-info btn-lg btn-block'
+              >
+                Edit Movie
+              </Link>
+              <br />
+            </div>
           </div>
-            {/* <br />
+          {/* <br />
             <button type="button" class="btn btn-outline-info btn-lg btn-block">Edit Movie</button>
             <button type="button" class="btn btn-outline-danger btn-lg btn-block">Delete Movie</button> */}
-
         </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = ({movieReducer}) => {
-  console.log(movieReducer)
-  const { movie }= movieReducer;
+const mapStateToProps = ({ movieReducer }) => {
+  console.log(movieReducer);
+  const { movie } = movieReducer;
 
   return {
-    movie
-  }
-}
+    movie,
+  };
+};
 
 const mapDispatchToProps = {
-  getMovie: movieActions.getMovie
-}
+  getMovie: movieActions.getMovie,
+};
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(showMovieDetails) ;
+export default connect(mapStateToProps, mapDispatchToProps)(showMovieDetails);
